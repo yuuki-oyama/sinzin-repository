c5540e2cd3dd856c00a33a0f3796d24aa25bfa4a
Yuki Oyama <yuuki.oyama@hibari-system.co.jp>
2025-09-09 17:38:18 +0900

    上に余白がないのが気にいらなかったので追加

--- resources/views/calc/index.blade.php
+++ resources/views/calc/index.blade.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="d-flex flex-column align-items-center gap-3">
-    <h1>計算アプリ</h1>
+    <h1 class="mt-5">計算アプリ</h1>
     {{-- フォームの中にほしい値を入れる --}}
     <form method="POST" action="/calc">
         <div class="d-flex">
